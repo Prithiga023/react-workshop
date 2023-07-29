e1f9b825f85a9292586adafe808ed6765759a9d7
Prithiga023 <129827381+Prithiga023@users.noreply.github.com>
2023-07-29 13:24:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+App.js
+import './App.css';
+import Header from './Header';
+import Counter from './Counter';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header title="rajalakshmi institute of technology"/>
+     <Counter/>
+    </div>
+   
+  );
+}
+
+export default App
